5097abba5c016bc3e47180c105fc235fc2b77c61
Nine <ninerattana2005@gmail.com>
2024-05-08 09:42:21 +0700

    store out_of_stock in linked list
    edited stock_alert and add datatype to AVL.h

--- AVL.h
+++ AVL.h
@@ -1,8 +1,13 @@
-#include<stdio.h>
-
-typedef struct AVL_Tree{
-    char ID[6]/*ไอดีสินค้า*/ , stockID[6]/*ไอ่ดีสต็อค*/ , imports[7]/*วันสินค้าเข้า[รูปแบบวันที่ DDMMYY]*/ , exports[7]/*วันสินค้าออก[รูปแบบวันที่ DDMMYY]*/;
-    int stock/*ของคงคลัง*/ , access/*จำนวนการกดดูสินค้านั้นๆ*/ , addToCart/*จำนวนการเพิ่มสินค้านั้นๆลงตระกร้า*/ , buy/*จำนวนการซื้อ(ครั้ง)*/ , heightOfTree/*สำหรับหมุนต้นไม้*/;
-    int key;/*ค่าที่เอาไว้วัดระดับการinsert มาจากaccess+addToCart+buy*/
-    struct AVL_Tree* left , * right;
-}AVL_Tree;
+#include <stdio.h>
+typedef struct AVL_Tree
+{
+    char ID[6] /*ไอดีสินค้า*/, stockID[6] /*ไอ่ดีสต็อค*/, imports[7] /*วันสินค้าเข้า[รูปแบบวันที่ DDMMYY]*/, exports[7] /*วันสินค้าออก[รูปแบบวันที่ DDMMYY]*/;
+    int stock /*ของคงคลัง*/, access /*จำนวนการกดดูสินค้านั้นๆ*/, addToCart /*จำนวนการเพิ่มสินค้านั้นๆลงตระกร้า*/, buy /*จำนวนการซื้อ(ครั้ง)*/, heightOfTree /*สำหรับหมุนต้นไม้*/;
+    int key; /*ค่าที่เอาไว้วัดระดับการinsert มาจากaccess+addToCart+buy*/
+    struct AVL_Tree *left, *right;
+} AVL_Tree;
+typedef struct out_of_stock_list
+{
+    char ID[6];
+    struct out_of_stock_list *next;
+} out_of_stock_list;
--- Stock.c
+++ Stock.c
@@ -25,51 +25,75 @@ int assume_out_of_stock_product(struct AVL_Tree *node){
 - ต้อง traversal พร้อมเช็คเรื่อยๆ ถ้าใช่ก็ใส่ลงใน arr
 - return เมื่อ traversal ครบเท่านั้น
 */
-char **stock_alert(struct AVL_Tree *node){
-    //check end
-    if( node == NULL)
-        return;
+// char **stock_alert(struct AVL_Tree *node){
+//     //check end
+//     if( node == NULL)
+//         return;
 
-    //2D array with char
-    /*
-    format out_of_stock_ID : {{AAAAA},{BBBBB}}
-    meaning stock AAAAA , BBBBB product in stock less than 10
-    */
-    int rows =assume_out_of_stock_product(node);
-    char **out_of_stock_ID = (char **)malloc(rows * sizeof(char *));
-    if (out_of_stock_ID == NULL) {
-        printf("Memory allocation failed.\n");
-        return NULL;
+//     //2D array with char
+//     /*
+//     format out_of_stock_ID : {{AAAAA},{BBBBB}}
+//     meaning stock AAAAA , BBBBB product in stock less than 10
+//     */
+//     int rows =assume_out_of_stock_product(node);
+//     char **out_of_stock_ID = (char **)malloc(rows * sizeof(char *));
+//     if (out_of_stock_ID == NULL) {
+//         printf("Memory allocation failed.\n");
+//         return NULL;
+//     }
+
+//     for (int i = 0; i < rows; i++) {
+//         out_of_stock_ID[i] = (char *)malloc(6/*stock id have only 5 character*/ * sizeof(char)); // +1 for the null terminator
+//         if (out_of_stock_ID[i] == NULL) {
+//             printf("Memory allocation failed.\n");
+//             // Free previously allocated memory to avoid memory leaks
+//             for (int j = 0; j < i; j++) {
+//                 free(out_of_stock_ID[j]);
+//             }
+//             free(out_of_stock_ID);
+//             return NULL;
+//         }
+//     }
+
+//     // check less than 10 to add to arr
+//     if( node->stock <= 10){
+//     }
+    
+// }
+//เก็บในรูปเเบบ linked list ของ out of stock เพิ่ม datatype ใหม่ใน AVL.h ชื่อ out_of_stock_list
+void stock_alert(AVL_Tree *tree, out_of_stock_list *head)
+{
+
+    if (tree->right != NULL)
+    {
+        stock_alert(tree->right, head);
     }
-
-    for (int i = 0; i < rows; i++) {
-        out_of_stock_ID[i] = (char *)malloc(6/*stock id have only 5 character*/ * sizeof(char)); // +1 for the null terminator
-        if (out_of_stock_ID[i] == NULL) {
-            printf("Memory allocation failed.\n");
-            // Free previously allocated memory to avoid memory leaks
-            for (int j = 0; j < i; j++) {
-                free(out_of_stock_ID[j]);
+    if (tree->stock <= 5)
+    {
+        printf("Out of stock ID = %s\n", tree->ID);
+        if (head == NULL)
+        {
+            out_of_stock_list *newNode = (out_of_stock_list *)malloc(sizeof(out_of_stock_list));
+            newNode->next = NULL;
+            strcpy(newNode->ID, tree->ID);
+            head = newNode;
+        }
+        else
+        {
+            out_of_stock_list *ptr = head;
+            while (ptr->next != NULL)
+            {
+                ptr = ptr->next;
             }
-            free(out_of_stock_ID);
-            return NULL;
+            out_of_stock_list *newNode = (out_of_stock_list *)malloc(sizeof(out_of_stock_list));
+            newNode->next = NULL;
+            strcpy(newNode->ID, tree->ID);
+            ptr->next = newNode;
         }
     }
-
-    // check less than 10 to add to arr
-    if( node->stock <= 10){
-    }
-    
-}
-void stock_alert(AVL_Tree *tree){
-    
-    if(tree->right != NULL){
-        stock_alert(tree->right);
-    }
-    if(tree->stock <= 5){
-        printf("Out of stock ID = %s\n",tree->ID);
-    }
-    if(tree->left != NULL){
-        stock_alert(tree->left);
+    if (tree->left != NULL)
+    {
+        stock_alert(tree->left, head);
     }
     return;
 }
